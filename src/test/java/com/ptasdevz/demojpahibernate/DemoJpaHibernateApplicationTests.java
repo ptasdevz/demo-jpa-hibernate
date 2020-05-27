bcb8d78289fdd6e96288e92bcaf7636d985d7c3f
Jason Peters <ptasdevz@gmail.com>
2020-05-27 16:36:33 -0400

    second commit

---
+++ src/test/java/com/ptasdevz/demojpahibernate/DemoJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptasdevz.demojpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
